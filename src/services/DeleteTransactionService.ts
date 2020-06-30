e3d54fd1d8734fff9b9c55e467f3e8e5ccd9731a
juliomchado <juliocarlos00@hotmail.com>
2020-06-30 19:37:10 +0100

    Project completed

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+
+import { getCustomRepository } from 'typeorm'
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const verifyExistis = await transactionRepository.findOne(id)
+
+    if (!verifyExistis) {
+      throw new Error('Transaction does not existis')
+    }
+
+    await transactionRepository.remove(verifyExistis)
   }
 }
 
